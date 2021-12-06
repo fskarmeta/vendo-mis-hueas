5d98fa36df8c72c39da85831902e740c80778251
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-12-05 21:46:19 -0300

    CHORE: Add ajv-l18n and import spanish locale

--- api-lib/middlewares/ajv.js
+++ api-lib/middlewares/ajv.js
@@ -1,4 +1,23 @@
 import Ajv from 'ajv';
+import localize_es from 'ajv-i18n/localize/es';
+
+const localeMap = {
+  name: 'Nombre',
+  email: 'Correo electrónico',
+  password: 'Contraseña',
+  confirmPassword: 'Confirmar contraseña',
+  phone: 'Teléfono',
+  address: 'Dirección',
+  city: 'Ciudad',
+  state: 'Estado',
+  zip: 'Código postal',
+  country: 'País',
+  bio: 'Biografía',
+  detail: 'Detalle',
+  description: 'Descripción',
+  oldPassword: 'Contraseña anterior',
+  newPassword: 'Nueva contraseña',
+};
 
 export function validateBody(schema) {
   const ajv = new Ajv();
@@ -8,10 +27,15 @@ export function validateBody(schema) {
     if (valid) {
       return next();
     } else {
+      localize_es(validate.errors);
       const error = validate.errors[0];
+      console.log(error);
       return res.status(400).json({
         error: {
-          message: `"${error.instancePath.substring(1)}" ${error.message}`,
+          message: `"${
+            localeMap[error.instancePath.substring(1)] ||
+            error.instancePath.substring(1)
+          }" ${error.message}`,
         },
       });
     }
