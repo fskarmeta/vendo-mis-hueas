2c5dfc60c4c7b42cab7d6aa2ac01e120f0f7e3d9
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-12-20 22:21:27 -0300

    WIP: Adding images to product front

--- components/Avatar/Avatar.jsx
+++ components/Avatar/Avatar.jsx
@@ -1,9 +1,9 @@
 import styles from './Avatar.module.css';
 
-const Avatar = ({ size, username, url }) => {
+const Avatar = ({ size, username, url, product }) => {
   return (
     <img
-      className={styles.avatar}
+      className={product ? styles.product: styles.avatar}
       src={url || '/images/default_user.jpg'}
       alt={username}
       width={size}
--- page-components/AddProduct/Index.jsx
+++ page-components/AddProduct/Index.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/Button';
+import { Avatar } from '@/components/Avatar';
 import { Input, Textarea } from '@/components/Input';
 import { Spacer } from '@/components/Layout';
 import Wrapper from '@/components/Layout/Wrapper';
@@ -11,6 +12,45 @@ export const AddProduct = () => {
   const title = useRef();
   const description = useRef();
   const detail = useRef();
+  const thumbnailPictureRef = useRef();
+  const productImages = useRef(['', '', '', '', '']);
+  const [thumbnailRef, setThumbnailRef] = useState('');
+  const [productImagesRefs, setProductImagesRefs] = useState([
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]);
+
+  const onThumbnailChange = useCallback((e) => {
+    const file = e.currentTarget.files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = (l) => {
+      setThumbnailRef(l.currentTarget.result);
+    };
+    reader.readAsDataURL(file);
+  }, []);
+
+  const onProductImageChange = (i) =>
+    useCallback(
+      (e) => {
+        console.log(e, i);
+        const file = e.currentTarget.files?.[0];
+        if (!file) return;
+        const reader = new FileReader();
+        const refsCopy = [...productImagesRefs];
+        console.log('refsCopt', refsCopy);
+        reader.onload = (l) => {
+          refsCopy[i] = l.currentTarget.result;
+          setProductImagesRefs(refsCopy);
+        };
+        reader.readAsDataURL(file);
+        console.log('imgaesref', productImages);
+      },
+      [productImagesRefs]
+    );
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -48,6 +88,45 @@ export const AddProduct = () => {
           <Spacer size={0.5} axis="vertical" />
           <Textarea ref={detail} label="Detalle" />
           <Spacer size={0.5} axis="vertical" />
+          <span className={styles.label}>Imágen miniatura del producto</span>
+          <div className={styles.avatar}>
+            <Avatar
+              size={96}
+              username="product-thumbnail"
+              url={thumbnailRef}
+              product
+            />
+            <input
+              aria-label="Imágen miniatura del producto"
+              type="file"
+              accept="image/*"
+              ref={thumbnailPictureRef}
+              onChange={onThumbnailChange}
+            />
+          </div>
+          <Spacer size={0.5} axis="vertical" />
+          <span className={styles.label}>Imágenes del producto</span>
+          <div className={styles.productImages}>
+            {productImagesRefs.map((image, i) => {
+              return (
+                <div className={styles.avatar} key={i}>
+                  <Avatar
+                    size={96}
+                    username="product-thumbnail"
+                    url={image}
+                    product
+                  />
+                  <input
+                    aria-label="Imágen miniatura del producto"
+                    type="file"
+                    accept="image/*"
+                    ref={(el) => (productImages.current[i] = el)}
+                    onChange={onProductImageChange(i)}
+                  />
+                </div>
+              );
+            })}
+          </div>
           <Spacer size={0.5} axis="vertical" />
           <Button
             htmlType="submit"
